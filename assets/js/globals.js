332ae54b71d7a3ed5452414f4d2cc47e28330cc3
Frostplexx <daniel.inama02@gmail.com>
2023-10-12 17:18:16 +0200

    Update bearerToken and tidy up code
    Updated the bearerToken in globals.js for updated access, reflecting new security measures taken on the server side. Also, made a minor adjustment in faq.html to tidy up the code and remove excess whitespace, improving readability.

--- assets/js/globals.js
+++ assets/js/globals.js
@@ -1,5 +1,4 @@
-export const bearerToken = "M2FLX3UrKyZzTkFMayQrWkt2NFE=";
+export const bearerToken = "3aK_u++&sNALk$+ZKv4Q";
 //export const baseUrl = "https://preview.webhook.metisbot.xyz";
 //export const baseUrl = "http://localhost:3000";
 export const baseUrl = "https://webhook.metisbot.xyz";
-
